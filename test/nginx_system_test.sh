9584018c93fee916d99ee479709695e10a793c5d
Otto van der Schaaf <oschaaf@we-amp.com>
2016-08-08 13:42:10 +0200

    special-responses: fix for hanging sometimes on special responses
    After calling ngx_http_filter_finalize_request return NGX_DONE
    as we are still in pre-access phase. This bug shows up when the
    "special" response is generated from, for example, php as opposed
    to a static file.
    
    Second part of the fix for
    https://github.com/pagespeed/ngx_pagespeed/issues/1146

--- test/nginx_system_test.sh
+++ test/nginx_system_test.sh
@@ -1315,6 +1315,12 @@ URL="http://noflush.example.com/mod_pagespeed_test/slow_flushing_html_response.p
 check_flushing "curl -N --raw --silent --proxy $SECONDARY_HOSTNAME $URL" \
   5.4 1
 
+start_test Special responses from php are handled OK.
+URL="http://special-response.example.com/A.foo.css.pagespeed.cf.0.css"
+OUT=$(http_proxy=$SECONDARY_HOSTNAME $WGET_DUMP -S --content-on-error $URL 2>&1) || true
+check_from "$OUT" fgrep -qi '404'
+check_from "$OUT" fgrep -q "PHP with a call to flush"
+
 start_test Shutting down.
 
 # Fire up some heavy load if ab is available to test a stressed shutdown
@@ -1397,6 +1403,7 @@ OUT=$(cat "$ERROR_LOG" \
     | grep -v "\\[warn\\].*Controller process .* exited with status code.*" \
     | grep -v "\\[warn\\].*Rewrite.*failed.*.pagespeed....0.foo.*" \
     | grep -v "\\[warn\\].*A.blue.css.*but cannot access the original.*" \
+    | grep -v "\\[warn\\].*special-response.*foo.css.*but cannot access the original.*" \
     || true)
 
 check [ -z "$OUT" ]
